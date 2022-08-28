aaf7800b0346003c4ee4992ca5fdbbbb86d59543
HAKI <90172133+Haki04@users.noreply.github.com>
2022-08-28 12:11:30 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
 
-const cari = document.querySelector('.cari');
-const img =  document.querySelector('.dua');
-const input =  document.querySelector('input');
-const span =  document.querySelector('.container p #span');
-const p =  document.querySelector('p');
-
-cari.addEventListener('click', ()=>{
-    const s =  document.querySelector('input#cari').value;
-
-     img.style.opacity = '1';
-     p.style.opacity = '1';
-     span.innerHTML = s;
+const btn = document.querySelector('button')
+const fix = document.querySelector('.fix')
+const audio = document.querySelector('audio')
+const span = document.querySelector('span.jumlah')
+const spanW = document.querySelector('span.waktu')
+let banyak = 0;
+btn.addEventListener('click', ()=>{
+    banyak++;
+    btn.classList.toggle('anim')
+    span.innerHTML = `Skor ${banyak}`;
+    
 })
+let count = 10;
+let st = setInterval(()=>{
+    spanW.innerHTML = `Waktu ${count}`;
+    count--;
+    if (count == 0) {
+        clearInterval(st);
+        spanW.innerHTML = `Waktu Anda Habis`;
+        
+        fix.style.width='100%';
+        fix.style.height='100vh';
+        
+        
+        const playdata = () => {      
+      const dataPlay = audio.getAttribute('data-Play');
+            
+      if(dataPlay === 'false') {
+       	audio.setAttribute('data-Play', 'true');
+       	audio.play()
+      }
+    }
+    playdata();
+    
+    }
+    
+}, 1000)
